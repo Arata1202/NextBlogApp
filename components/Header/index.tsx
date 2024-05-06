eca3588cd04656b674e81d5aa64e12c1e96e0bfa
Arata <aratadayoon@yahoo.co.jp>
2024-05-06 17:13:18 +0900

    update

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -6,7 +6,13 @@ import Link from 'next/link';
 import styles from './index.module.css';
 import { Dialog, Popover, Transition } from '@headlessui/react';
 import { Bars3Icon, XMarkIcon, ChevronDownIcon } from '@heroicons/react/24/outline';
-import { HomeIcon, UserCircleIcon, EnvelopeIcon, FolderIcon } from '@heroicons/react/24/solid';
+import {
+  HomeIcon,
+  UserCircleIcon,
+  EnvelopeIcon,
+  FolderIcon,
+  FolderOpenIcon,
+} from '@heroicons/react/24/solid';
 
 const headerNavigation = [
   { name: 'ホーム', href: '/', icon: HomeIcon },
@@ -26,8 +32,8 @@ const mobileBottomNavigation = [
 ];
 
 const categories = [
-  { name: '大学生活', href: '/tags/university' },
-  { name: 'プログラミング', href: '/tags/programming' },
+  { name: '大学生活', href: '/tags/university', icon: FolderOpenIcon },
+  { name: 'プログラミング', href: '/tags/programming', icon: FolderOpenIcon },
 ];
 
 export default function Header() {
@@ -113,100 +119,84 @@ export default function Header() {
           </Popover>
         </div>
       </nav>
-      <Dialog className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-        <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="hamburger fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-              <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt=""
-              />
-            </a>
-            <button
-              type="button"
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
-              onClick={() => setMobileMenuOpen(false)}
-            >
-              <span className="sr-only">Close menu</span>
-              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-            </button>
-          </div>
-          <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
-                <h1 className="text-2xl bg-gray-300 font-bold text-center mt-4 p-2">Menu</h1>
-                {mobileTopNavigation.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    className="mt-10 flex text-lg leading-6 text-gray-900 hover:text-blue-500 border-b border-gray-300 font-bold p-2"
-                  >
-                    {/* <item.icon className="h-5 w-5 mr-2 my-4" aria-hidden="true" /> */}
-                    ▶︎ {item.name}
-                  </a>
-                ))}
-                <div className="mt-10 flex text-lg leading-6 text-gray-900 border-b border-gray-300 font-bold p-2">
-                  ▶︎ カテゴリー
-                </div>
-                {categories.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    className="ml-5 mt-10 flex text-lg leading-6 text-gray-900 hover:text-blue-500 border-b border-gray-300 font-bold p-2"
-                  >
-                    {/* <item.icon className="h-5 w-5 mr-2 my-4" aria-hidden="true" /> */}＞{' '}
-                    {item.name}
-                  </a>
-                ))}
-                {mobileBottomNavigation.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    className="mt-10 flex text-lg leading-6 text-gray-900 hover:text-blue-500 border-b border-gray-300 font-bold p-2"
-                  >
-                    {/* <item.icon className="h-5 w-5 mr-2 my-4" aria-hidden="true" /> */}
-                    ▶︎ {item.name}
-                  </a>
-                ))}
-                {/* <Popover className="relative">
-                  <Popover.Button className="flex items-center text-sm font-medium text-gray-900 hover:text-blue-500 focus:outline-none">
-                    <FolderIcon className="h-5 w-5 mr-2" aria-hidden="true" />
-                    <b className="text-2xl my-3">▶︎ カテゴリー</b>
-                    <ChevronDownIcon className="ml-1 h-5 w-5 text-gray-400" aria-hidden="true" />
-                  </Popover.Button>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-200"
-                    enterFrom="opacity-0 translate-y-1"
-                    enterTo="opacity-100 translate-y-0"
-                    leave="transition ease-in duration-150"
-                    leaveFrom="opacity-100 translate-y-0"
-                    leaveTo="opacity-0 translate-y-1"
-                  >
-                    <Popover.Panel className="absolute z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <div className="py-1">
-                        {categories.map((category) => (
-                          <Link
-                            key={category.name}
-                            href={category.href}
-                            className="block px-4 py-2 text-lg text-gray-700 hover:bg-gray-100"
-                            onClick={() => setMobileMenuOpen(false)}
-                          >
-                            {category.name}
-                          </Link>
-                        ))}
-                      </div>
-                    </Popover.Panel>
-                  </Transition>
-                </Popover> */}
+      <Transition.Root show={mobileMenuOpen} as={Fragment}>
+        <Dialog as="div" className="relative lg:hidden z-50" onClose={setMobileMenuOpen}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-in-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in-out duration-300"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+
+          <Transition.Child
+            as={Fragment}
+            enter="ease-in-out duration-500 transform"
+            enterFrom="translate-x-full"
+            enterTo="translate-x-0"
+            leave="ease-in-out duration-500 transform"
+            leaveFrom="translate-x-0"
+            leaveTo="translate-x-full"
+          >
+            <Dialog.Panel className="fixed inset-y-0 right-0 flex max-w-xs w-full bg-white shadow-xl">
+              <div className="flex w-full flex-col p-5">
+                <button
+                  type="button"
+                  className="-ml-2 flex items-center justify-end p-2"
+                  onClick={() => setMobileMenuOpen(false)}
+                >
+                  <XMarkIcon className="h-6 w-6 text-gray-900" aria-hidden="true" />
+                  <span className="sr-only">Close menu</span>
+                </button>
+                <ul className="mt-5 space-y-6">
+                  {mobileTopNavigation.map((item) => (
+                    <li key={item.name}>
+                      <Link href={item.href}>
+                        <div className="flex items-center py-1 text-base font-bold text-gray-900 border-b border-gray-300 hover:text-blue-500">
+                          <div>▶︎</div>
+                          <item.icon className="h-6 w-6 mr-2 ml-2" aria-hidden="true" />
+                          {item.name}
+                        </div>
+                      </Link>
+                    </li>
+                  ))}
+                  <div className="flex items-center py-1 text-base font-bold text-gray-900 border-b border-gray-300 hover:text-blue-500">
+                    <div>▼</div>
+                    <FolderIcon className="h-6 w-6 mr-2 ml-2" aria-hidden="true" />
+                    カテゴリー
+                  </div>
+                  {categories.map((item) => (
+                    <li key={item.name}>
+                      <Link href={item.href}>
+                        <div className="ml-5 flex items-center py-1 text-base font-medium text-gray-900 border-b border-gray-300 hover:text-blue-500">
+                          <div>{'>'}</div>
+                          <item.icon className="h-6 w-6 mr-2 ml-2" aria-hidden="true" />
+                          {item.name}
+                        </div>
+                      </Link>
+                    </li>
+                  ))}
+                  {mobileBottomNavigation.map((item) => (
+                    <li key={item.name}>
+                      <Link href={item.href}>
+                        <div className="flex items-center py-1 text-base font-bold text-gray-900 border-b border-gray-300 hover:text-blue-500">
+                          <div>▶︎</div>
+                          <item.icon className="h-6 w-6 mr-2 ml-2" aria-hidden="true" />
+                          {item.name}
+                        </div>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
               </div>
-            </div>
-          </div>
-        </Dialog.Panel>
-      </Dialog>
+            </Dialog.Panel>
+          </Transition.Child>
+        </Dialog>
+      </Transition.Root>
     </header>
   );
 }
