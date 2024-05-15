5c5c6c624219f6c0c8c241b65e454ac44ea3f82e
Arata <aratadayoon@yahoo.co.jp>
2024-05-15 22:17:39 +0900

    fix pagination

--- app/p/[current]/page.tsx
+++ app/p/[current]/page.tsx
@@ -2,6 +2,7 @@ import { getList } from '@/libs/microcms';
 import { LIMIT } from '@/constants';
 import Pagination from '@/components/Pagination';
 import ArticleList from '@/components/ArticleList';
+import { BellAlertIcon } from '@heroicons/react/24/solid';
 
 type Props = {
   params: {
@@ -19,6 +20,12 @@ export default async function Page({ params }: Props) {
   });
   return (
     <>
+      <h1 className="categoryTitle text-3xl font-bold pt-5 max-w-[85rem] sm:px-6 lg:px-8 mx-auto pb-2">
+        <div className="flex items-center pb-2 pt-2">
+          <BellAlertIcon className="h-8 w-8 mr-2" aria-hidden="true" />
+          <div>最新記事</div>
+        </div>
+      </h1>
       <ArticleList articles={data.contents} />
       <Pagination totalCount={data.totalCount} current={current} />
     </>
