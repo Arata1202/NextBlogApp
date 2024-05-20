64babda297e4de380c0160d0a65f9e90fc8e677d
Arata <aratadayoon@yahoo.co.jp>
2024-05-21 02:39:19 +0900

    Revert "add golang backend"
    This reverts commit 82db4064b9b457dc672893e0210b09cd750fb402.

--- api/go.mod
+++
@@ -1,5 +0,0 @@
-module NextBlogApp
-
-go 1.22.3
-
-require github.com/joho/godotenv v1.5.1
--- components/Fixed/contact.tsx
+++ components/Fixed/contact.tsx
@@ -62,7 +62,7 @@ const ContactPage: React.FC<{ sidebarArticles: any }> = ({ sidebarArticles }) =>
   const sendEmail = useCallback(() => {
     if (!formData) return;
 
-    fetch('https://realunivlog.com/api/email', {
+    fetch('https://microcmsblog.aratasportfolio.com/email.php', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(formData),
@@ -86,7 +86,7 @@ const ContactPage: React.FC<{ sidebarArticles: any }> = ({ sidebarArticles }) =>
   const handleConfirmSend = useCallback(() => {
     const verifyCaptcha = async () => {
       try {
-        const response = await fetch('https://realunivlog.com/api/recaptcha', {
+        const response = await fetch('https://microcmsblog.aratasportfolio.com/recaptcha.php', {
           method: 'POST',
           headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
           body: `g-recaptcha-response=${captchaValue}`,
