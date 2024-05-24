02cd25aca8d1d1bd755de5a60a689a1f4136ebae
Arata <aratadayoon@yahoo.co.jp>
2024-05-25 07:45:41 +0900

    Revert "キャッシュ"
    This reverts commit 7cfd0e348fcdba97c0d2f5fd6a0df34474107705.

--- next.config.js
+++ next.config.js
@@ -14,31 +14,6 @@ module.exports = withPWA({
 });
 
 module.exports = {
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
-          },
-          {
-            key: 'Pragma',
-            value: 'no-cache',
-          },
-          {
-            key: 'Expires',
-            value: '0',
-          },
-          {
-            key: 'Surrogate-Control',
-            value: 'no-store',
-          },
-        ],
-      },
-    ];
-  },
   images: {
     domains: ['images.microcms-assets.io'],
     remotePatterns: [
