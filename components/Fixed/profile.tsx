d06fdc996643b97567d3fbf9e8503a47db29be81
Arata <aratadayoon@yahoo.co.jp>
2024-05-28 17:20:35 +0900

    profile強化

--- components/Fixed/profile.tsx
+++ components/Fixed/profile.tsx
@@ -7,6 +7,7 @@ import { UserCircleIcon } from '@heroicons/react/20/solid';
 import Sidebar from '@/components/Sidebar';
 import Share from '../Share';
 import AdAlert from '../AdAlert';
+import Link from 'next/link';
 
 const ProfilePage: React.FC<{ sidebarArticles: any }> = ({ sidebarArticles }) => {
   //出稿日
@@ -54,7 +55,7 @@ const ProfilePage: React.FC<{ sidebarArticles: any }> = ({ sidebarArticles }) =>
                     「プログラミングを勉強する大学生」をテーマにブログを始めました。
                     <br />
                     <br />
-                    大学では経営やマーケティングについて研究していますが、独学とインターンでプログラミングの学習も進めています。プログラミングは完全未経験から学習しましたが、わずか１か月で習得することが出来ました。
+                    大学では経営やマーケティングについて研究していますが、並行して独学とインターンでプログラミングの学習も進めています。
                     <br />
                     <br />
                     同様に未経験からエンジニアを目指すといった方も多いと思います。
@@ -69,24 +70,51 @@ const ProfilePage: React.FC<{ sidebarArticles: any }> = ({ sidebarArticles }) =>
                   <ul>
                     <li>年齢｜21歳</li>
                     <li>在住｜千葉県</li>
-                    <li>大学生｜マーケティング学科</li>
-                    <li>インターン｜Webエンジニア</li>
+                    <li>大学生｜東洋大学 経営学部 マーケティング学科</li>
+                    <li>インターン｜Webエンジニア（長期）</li>
                     <li>趣味｜旅行、ドライブ、ゲーム、プログラミングなどなど</li>
                   </ul>
-                  <h2>このブログについて</h2>
+                  <h2>ブログ始めたきっかけ</h2>
                   <p>
-                    まだまだ大学２年生ですが、様々な経験をしてきました。社会人になる一歩手前ということもあり、挑戦できることの幅も増え、運転免許を取得したり、アルバイトを始めたり、将来の夢に向けて勉強したりなど、やらなければならない事がたくさん増えました。親ともめるなんてこともしょっちゅうです。。
+                    まだまだ大学２年生ですが、様々な経験をしてきました。社会人になる一歩手前ということもあり、挑戦できることの幅も増え、運転免許を取得したり、アルバイトを始めたり、将来の夢に向けて勉強したりなど、やらなければならない事がたくさん増えました。
                     <br />
                     <br />
-                    そんな中、自分なりに解決法を探しながら生活してきました。それらの知識を無駄にせず、同じ気持ちを抱く大学生の方々に共有したいと考え、ブログを始めました。
+                    そんな中、自分なりに解決法を探しながら生活してきました。それらの経験を無駄にせず、同じ気持ちを抱く大学生の方々に共有したいと考え、ブログを始めました。
                   </p>
                   <h2>技術スタック</h2>
-                  <p>こちらのブログは以下の構成で、運営しています。</p>
+                  <p>筆者は以下の技術をよく使用しています。</p>
                   <ul>
-                    <li>フロントエンド｜Next.js</li>
-                    <li>インフラ｜Vercel</li>
-                    <li>CMS｜MicroCMS</li>
+                    <li>フロントエンド｜React, Next.js, Vue.js, TypeScript, TailwindCSS</li>
+                    <li>バックエンド｜PHP, Laravel</li>
+                    <li>データベース｜MySQL</li>
+                    <li>ホスティング｜Vercel, AWS, Xserver</li>
+                    <li>ツール｜GitHub, Docker</li>
                   </ul>
+                  <p>
+                    インターンではVue.jsとLaravel、個人開発ではReactやNext.js,
+                    PHPの使用頻度が高いです。
+                  </p>
+                  <h2>当ブログ（リアル大学生）の使用技術</h2>
+                  <p></p>
+                  <p>当ブログ（リアル大学生）は以下の構成で、運営しています。</p>
+                  <ul>
+                    <li>フロントエンド｜Next.js, TypeScript, TailwindCSS</li>
+                    <li>ツール｜MicroCMS</li>
+                    <li>ホスティング｜Vercel</li>
+                    <li>バックエンド｜PHP, FTP, Xserver</li>
+                  </ul>
+                  <h2>お問い合わせ</h2>
+                  <p>
+                    お問い合わせの際は、下記の窓口からお願いいたします。
+                    <br />
+                    <br />
+                    サイト運営者：あお
+                    <br />
+                    連絡先：
+                    <Link href="/contact" className="text-blue-500 hover:text-blue-700">
+                      https://realunivlog.com/contact/
+                    </Link>
+                  </p>
                 </div>
                 <Share />
               </div>
