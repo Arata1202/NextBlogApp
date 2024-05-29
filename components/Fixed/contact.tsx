9943866a4eb26c27d07f1ef439084b49d304c899
Arata <aratadayoon@yahoo.co.jp>
2024-05-29 22:10:24 +0900

    パンクズ

--- components/Fixed/contact.tsx
+++ components/Fixed/contact.tsx
@@ -4,7 +4,7 @@ import React, { useState, useEffect, useRef, useCallback, Fragment } from 'react
 import { useForm } from 'react-hook-form';
 import { Dialog, Transition } from '@headlessui/react';
 import { XMarkIcon, CheckCircleIcon } from '@heroicons/react/24/outline';
-import { EnvelopeIcon } from '@heroicons/react/20/solid';
+import { EnvelopeIcon, HomeIcon, ChevronRightIcon } from '@heroicons/react/24/solid';
 import ReCAPTCHA from 'react-google-recaptcha';
 import Share from '../Share';
 import AdAlert from '../AdAlert';
@@ -125,7 +125,31 @@ const ContactPage: React.FC<{ sidebarArticles: any }> = ({ sidebarArticles }) =>
       <Header />
       <main className={styles.main}>
         <div className="categoryTitle max-w-[85rem] sm:px-6 lg:px-8 mx-auto pb-2">
-          <div className="flex items-center py-2 mt-2">
+          <nav className="flex" aria-label="Breadcrumb">
+            <ol role="list" className="flex items-center space-x-4">
+              <li>
+                <a href="/" className="flex text-gray-500 hover:text-blue-500">
+                  <HomeIcon className="h-4 w-4 flex-shrink-0" aria-hidden="true" />
+                  <div className="ml-1 text-sm font-medium">ホーム</div>
+                </a>
+              </li>
+              <li>
+                <div className="flex items-center">
+                  <ChevronRightIcon
+                    className="h-4 w-4 flex-shrink-0 text-gray-400"
+                    aria-hidden="true"
+                  />
+                  <a
+                    href="/contact"
+                    className="ml-4 text-sm font-medium text-gray-500 hover:text-blue-500"
+                  >
+                    お問い合わせ
+                  </a>
+                </div>
+              </li>
+            </ol>
+          </nav>
+          <div className="flex items-center py-2 mt-7">
             <EnvelopeIcon className="h-8 w-8 mr-2" aria-hidden="true" />
             <h1 className="text-3xl font-bold lg:text-3xl">お問い合わせ</h1>
           </div>
