5404b883d5f15f4cb45aa8b6a5b94ff107db1523
Arata <aratadayoon@yahoo.co.jp>
2024-06-18 01:49:37 +0900

    Revert "add ad"
    This reverts commit b5d27d818ed9dcb62b3a728338cc731bd12d5df1.

--- components/Article/index.tsx
+++ components/Article/index.tsx
@@ -90,28 +90,12 @@ const formatRichText = (richText: string) => {
       return hljs.highlightAuto(text);
     }
   };
-
-  const adCode = `
-    <div class="ad-container">
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1705865999592590" crossorigin="anonymous"></script>
-      <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-1705865999592590" data-ad-slot="7197259627" data-ad-format="auto" data-full-width-responsive="true"></ins>
-      <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-      </script>
-    </div>
-  `;
-
   $('pre code').each((_, elm) => {
     const lang = $(elm).attr('class');
     const res = highlight($(elm).text(), lang);
     $(elm).html(res.value);
     $(elm).addClass('hljs');
   });
-
-  $('h2').each((_, elm) => {
-    $(elm).before(adCode);
-  });
-
   return $.html();
 };
 
