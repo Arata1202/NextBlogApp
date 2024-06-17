fcbd1cd836d15b19898eb45b6d225612df181f24
Arata <aratadayoon@yahoo.co.jp>
2024-06-18 01:02:49 +0900

    add ad

--- components/Article/index.tsx
+++ components/Article/index.tsx
@@ -96,6 +96,23 @@ const formatRichText = (richText: string) => {
     $(elm).html(res.value);
     $(elm).addClass('hljs');
   });
+
+  $('h2').each((index, element) => {
+    $(element).before(`
+      <div class="ad-container">
+        <ins class="adsbygoogle"
+          style="display:block"
+          data-ad-client="ca-pub-1705865999592590"
+          data-ad-slot="1234567890"
+          data-ad-format="auto"
+          data-full-width-responsive="true"></ins>
+        <script>
+          (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+      </div>
+    `);
+  });
+
   return $.html();
 };
 
