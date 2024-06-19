6b9dac7dcd44f8200ea3f18394055cde0cf795c1
Arata <aratadayoon@yahoo.co.jp>
2024-06-19 13:18:48 +0900

    Revert "adtest"
    This reverts commit 9f3b101f06412c1c61835d1bff6372039a41b5b5.

--- components/Article/index.tsx
+++ components/Article/index.tsx
@@ -70,12 +70,6 @@ hljs.registerLanguage('sql', sql);
 hljs.registerLanguage('typescript', typescript);
 hljs.registerLanguage('vim', vim);
 
-declare global {
-  interface Window {
-    adsbygoogle: any[];
-  }
-}
-
 interface Heading {
   id: string;
   title: string;
@@ -135,19 +129,6 @@ function useExtractHeadings(contentBlocks: { rich_text2?: string }[]): Heading[]
 
 export default function Article({ data, articles }: Props) {
   const headings = useExtractHeadings(data.content_blocks);
-
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.async = true;
-    script.src =
-      'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1705865999592590';
-    script.crossOrigin = 'anonymous';
-    document.body.appendChild(script);
-
-    script.onload = () => {
-      (window.adsbygoogle = window.adsbygoogle || []).push({});
-    };
-  }, []);
 
   return (
     <>
