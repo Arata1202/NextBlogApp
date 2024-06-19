4a2352aca0836f941351195a8a95f26bc91e97c0
Arata <aratadayoon@yahoo.co.jp>
2024-06-19 15:04:05 +0900

    fix ad

--- app/tag/[tagId]/page.tsx
+++ app/tag/[tagId]/page.tsx
@@ -26,6 +26,7 @@ export default async function Page({ params }: Props) {
   return (
     <>
       <ArticleList articles={data.contents} allArticles={data2.contents} />
+      <Pagination totalCount={data.totalCount} basePath={`/tag/${tagId}`} />
       <div style={{ maxWidth: '100%', overflow: 'hidden' }}>
         <ins
           className="adsbygoogle"
@@ -41,7 +42,6 @@ export default async function Page({ params }: Props) {
         `}
         </Script>
       </div>
-      <Pagination totalCount={data.totalCount} basePath={`/tag/${tagId}`} />
       <div className="pc">
         <TopSidebar articles={data2.contents} />
       </div>
