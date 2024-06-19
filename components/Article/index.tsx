b295668be3cdbd4a2ea882b74b351b4d2db2468a
Arata <aratadayoon@yahoo.co.jp>
2024-06-19 15:40:12 +0900

    ad test

--- components/Article/index.tsx
+++ components/Article/index.tsx
@@ -435,6 +435,21 @@ export default function Article({ data, articles }: Props) {
                     </div>
                   ))}
                 </div>
+                <div style={{ maxWidth: '100%', overflow: 'hidden' }}>
+                  <ins
+                    className="adsbygoogle"
+                    style={{ display: 'block', width: '100%' }}
+                    data-ad-client="ca-pub-1705865999592590"
+                    data-ad-slot="5469260892"
+                    data-ad-format="autorelaxed"
+                    data-full-width-responsive="false"
+                  ></ins>
+                  <Script id="adsbygoogle-init" strategy="afterInteractive">
+                    {`
+          (adsbygoogle = window.adsbygoogle || []).push({});
+        `}
+                  </Script>
+                </div>
                 <div className="related-articles mt-10">
                   <h1
                     className={`${styles.profile} text-2xl font-semibold flex justify-center pt-10`}
