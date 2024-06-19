4b56a396d184f555d52f795cc052116b693928ad
Arata <aratadayoon@yahoo.co.jp>
2024-06-19 15:07:18 +0900

    Revert "fix ad"
    This reverts commit 40952de39093755a71d62c8abc91d24b159b8501.

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import Pagination from '@/components/Pagination';
 import ArticleList from '@/components/ArticleList';
 import TopSidebar from '@/components/TopSidebar';
 import { BellAlertIcon } from '@heroicons/react/24/solid';
-import Script from 'next/script';
 
 export const revalidate = 60;
 
@@ -23,21 +22,6 @@ export default async function Page() {
       </h1>
       <ArticleList articles={data.contents} />
       <Pagination totalCount={data.totalCount} />
-      <div style={{ maxWidth: '100%', overflow: 'hidden' }}>
-        <ins
-          className="adsbygoogle"
-          style={{ display: 'block', width: '100%' }}
-          data-ad-client="ca-pub-1705865999592590"
-          data-ad-slot="7197259627"
-          data-ad-format="auto"
-          data-full-width-responsive="false"
-        ></ins>
-        <Script id="adsbygoogle-init" strategy="afterInteractive">
-          {`
-          (adsbygoogle = window.adsbygoogle || []).push({});
-        `}
-        </Script>
-      </div>
       <div className="pc">
         <TopSidebar articles={data.contents} />
       </div>
--- app/tag/[tagId]/page.tsx
+++ app/tag/[tagId]/page.tsx
@@ -26,7 +26,6 @@ export default async function Page({ params }: Props) {
   return (
     <>
       <ArticleList articles={data.contents} allArticles={data2.contents} />
-      <Pagination totalCount={data.totalCount} basePath={`/tag/${tagId}`} />
       <div style={{ maxWidth: '100%', overflow: 'hidden' }}>
         <ins
           className="adsbygoogle"
@@ -42,6 +41,7 @@ export default async function Page({ params }: Props) {
         `}
         </Script>
       </div>
+      <Pagination totalCount={data.totalCount} basePath={`/tag/${tagId}`} />
       <div className="pc">
         <TopSidebar articles={data2.contents} />
       </div>
