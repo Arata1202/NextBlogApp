87c14ab75c54a5920753038e40f79a7efcc52550
Arata <aratadayoon@yahoo.co.jp>
2024-06-19 14:50:20 +0900

    repair

--- components/Article/index.tsx
+++ components/Article/index.tsx
@@ -435,21 +435,6 @@ export default function Article({ data, articles }: Props) {
                     </div>
                   ))}
                 </div>
-                <div style={{ maxWidth: '100%', overflow: 'hidden' }}>
-                  <ins
-                    className="adsbygoogle"
-                    style={{ display: 'block', width: '100%' }}
-                    data-ad-client="ca-pub-1705865999592590"
-                    data-ad-slot="7197259627"
-                    data-ad-format="auto"
-                    data-full-width-responsive="false"
-                  ></ins>
-                  <Script id="adsbygoogle-init" strategy="afterInteractive">
-                    {`
-          (adsbygoogle = window.adsbygoogle || []).push({});
-        `}
-                  </Script>
-                </div>
                 <div className="related-articles mt-10">
                   <h1
                     className={`${styles.profile} text-2xl font-semibold flex justify-center pt-10`}
