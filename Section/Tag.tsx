01bfab6136265a1aeed9441b4023af83ab858f41
Arata <aratadayoon@yahoo.co.jp>
2024-06-20 11:35:25 +0900

    add tag category

--- Section/Tag.tsx
+++ Section/Tag.tsx
@@ -1,4 +1,6 @@
 export const tags = [
   { name: 'Git', link: '/tag/git' },
   { name: 'SEO対策', link: '/tag/seo' },
+  { name: 'Google AdSense', link: '/tag/adsense' },
+  { name: 'MicroCMS', link: '/tag/microcms' },
 ];
