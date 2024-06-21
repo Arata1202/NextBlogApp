ba9d2f5ca84c84e9d4ede2ae1453dae61a340374
Arata <aratadayoon@yahoo.co.jp>
2024-06-22 08:26:48 +0900

    #119 fix: Instagram

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -257,7 +257,7 @@ export default function Sidebar({ articles }: Props) {
           </div>
         </div>
 
-        <div className="flex justify-center">
+        <div className="flex justify-center mt-5">
           <blockquote
             className="instagram-media"
             data-instgrm-permalink="https://www.instagram.com/p/C2maqCNPUvg/?utm_source=ig_embed&amp;utm_campaign=loading"
@@ -268,7 +268,6 @@ export default function Sidebar({ articles }: Props) {
               borderRadius: '0px',
               boxShadow: '0 0 1px 0',
               margin: '1px',
-              marginTop: '20px',
               maxWidth: '540px',
               minWidth: '326px',
               padding: '0',
