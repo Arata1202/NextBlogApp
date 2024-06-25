a8e5d3faa04913841e3096046577b1ad541a4b34
Arata <aratadayoon@yahoo.co.jp>
2024-06-25 09:03:37 +0900

    lambda recaptchaテスト

--- components/Fixed/contact.tsx
+++ components/Fixed/contact.tsx
@@ -86,11 +86,14 @@ const ContactPage: React.FC<{ sidebarArticles: any }> = ({ sidebarArticles }) =>
   const handleConfirmSend = useCallback(() => {
     const verifyCaptcha = async () => {
       try {
-        const response = await fetch('https://microcmsblog.aratasportfolio.com/recaptcha.php', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-          body: `g-recaptcha-response=${captchaValue}`,
-        });
+        const response = await fetch(
+          '${process.env.NEXT_PUBLIC_LAMBDA_RECAPTCHA_ENDPOINT}',
+          {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: `g-recaptcha-response=${captchaValue}`,
+          },
+        );
         const data = await response.json();
         if (data.success) {
           sendEmail();
