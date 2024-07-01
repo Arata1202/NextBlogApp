84e543ca01f79b2755052e8e65ed847a90b70c44
Arata <aratadayoon@yahoo.co.jp>
2024-07-01 10:56:59 +0900

    feat: ad sitemap

--- components/ArticleList/index.tsx
+++ components/ArticleList/index.tsx
@@ -136,21 +136,6 @@ const ArticleList = ({ articles, allArticles }: Props) => {
               ))}
             </div>
           </ul>
-          {/* <div style={{ maxWidth: '100%', overflow: 'hidden' }}>
-            <ins
-              className="adsbygoogle"
-              style={{ display: 'block', width: '100%' }}
-              data-ad-client="ca-pub-1705865999592590"
-              data-ad-slot="7197259627"
-              data-ad-format="auto"
-              data-full-width-responsive="false"
-            ></ins>
-            <Script id="adsbygoogle-init" strategy="afterInteractive">
-              {`
-          (adsbygoogle = window.adsbygoogle || []).push({});
-        `}
-            </Script>
-          </div> */}
           <Share />
         </div>
         <div className="mobile">
