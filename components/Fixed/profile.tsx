aed3b15262c6dba77f2aac771bf370c44451d2da
Arata <aratadayoon@yahoo.co.jp>
2024-07-01 10:39:47 +0900

    test: ad profile

--- components/Fixed/profile.tsx
+++ components/Fixed/profile.tsx
@@ -184,11 +184,6 @@ const ProfilePage: React.FC<{ sidebarArticles: any }> = ({ sidebarArticles }) =>
                     data-ad-format="auto"
                     data-full-width-responsive="false"
                   ></ins>
-                  <Script id="adsbygoogle-init" strategy="afterInteractive">
-                    {`
-          (adsbygoogle = window.adsbygoogle || []).push({});
-        `}
-                  </Script>
                 </div>
                 <Share />
               </div>
