3e24e937e90ed7262ec3bb1a919494e1828646b3
Arata <aratadayoon@yahoo.co.jp>
2024-07-01 11:17:19 +0900

    feat: mulchplex article

---
+++ components/Adsense/mulchplex.tsx
@@ -0,0 +1,14 @@
+export default function Mulchplex() {
+  return (
+    <div style={{ maxWidth: '100%', overflow: 'hidden' }}>
+      <ins
+        className="adsbygoogle"
+        style={{ display: 'block', width: '100%' }}
+        data-ad-client="ca-pub-1705865999592590"
+        data-ad-slot="5469260892"
+        data-ad-format="autorelaxed"
+        data-full-width-responsive="false"
+      ></ins>
+    </div>
+  );
+}
