092ea421d00f3c613369136d5b8ddf122f06fbba
Arata <aratadayoon@yahoo.co.jp>
2024-07-01 10:52:25 +0900

    test: ad profile

--- components/Fixed/profile.tsx
+++ components/Fixed/profile.tsx
@@ -7,7 +7,7 @@ import FixedSidebar from '@/components/FixedSidebar';
 import Share from '../Share';
 import AdAlert from '../AdAlert';
 import Link from 'next/link';
-import Script from 'next/script';
+import Display from '../Adsense/display';
 
 const ProfilePage: React.FC<{ sidebarArticles: any }> = ({ sidebarArticles }) => {
   //出稿日
@@ -54,6 +54,7 @@ const ProfilePage: React.FC<{ sidebarArticles: any }> = ({ sidebarArticles }) =>
             <div className="lg:col-span-2">
               <div className="">
                 <div className="space-y-5 lg:space-y-8">
+                  <Display />
                   <div className="includeBanner flex justify-end gap-x-5">
                     {/* <TagList tags={data.tags} /> */}
                     <PublishedDate date={formattedDate} />
@@ -175,21 +176,7 @@ const ProfilePage: React.FC<{ sidebarArticles: any }> = ({ sidebarArticles }) =>
                     </Link>
                   </p>
                 </div>
-                <div style={{ maxWidth: '100%', overflow: 'hidden' }}>
-                  <ins
-                    className="adsbygoogle"
-                    style={{ display: 'block', width: '100%' }}
-                    data-ad-client="ca-pub-1705865999592590"
-                    data-ad-slot="7197259627"
-                    data-ad-format="auto"
-                    data-full-width-responsive="false"
-                  ></ins>
-                  <Script id="adsbygoogle-init" strategy="afterInteractive">
-                    {`
-          (adsbygoogle = window.adsbygoogle || []).push({});
-        `}
-                  </Script>
-                </div>
+                <Display />
                 <Share />
               </div>
             </div>
