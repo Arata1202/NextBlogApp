8d7f95cb4fa06dd69fd010c948940977635e453e
Arata <aratadayoon@yahoo.co.jp>
2024-07-01 10:17:09 +0900

    feat: adtest

--- components/ArticleListItem/index.tsx
+++ components/ArticleListItem/index.tsx
@@ -29,41 +29,52 @@ const ArticleListItem = ({ article }: Props) => {
   };
 
   return (
-    <li className={styles.list}>
-      <a
-        onClick={handleClick}
-        href={`/articles/${article.id}`}
-        className={`${styles.link} p-2 border border-gray-300 shadow-lg hover:shadow-xl transition-shadow duration-200 transform hover:-translate-y-1`}
-      >
-        <picture>
-          {isThumbnailAvailable && (
-            <>
-              <source type="image/webp" media="(max-width: 640px)" srcSet={mobileSrcSet} />
-              <source type="image/webp" srcSet={desktopSrcSet} />
-            </>
-          )}
-          <Image
-            src={imageSrc}
-            alt="サムネイル"
-            className={styles.image}
-            width="600"
-            height="300"
-            placeholder="blur"
-            blurDataURL={imageSrc}
-          />
-        </picture>
-        <div className={styles.content}>
-          <div className={styles.title}>{article.title}</div>
-          <div className={styles.description}>{article.description}</div>
-          <div className={styles.date}>
-            <FolderIcon className="h-5 w-5 mr-2 mt-4" aria-hidden="true" />
-            <TagList tags={article.tags} hasLink={false} />
-            &nbsp;&nbsp;&nbsp;&nbsp;
-            <PublishedDate date={article.publishedAt || article.createdAt} />
+    <>
+      <li className={styles.list}>
+        <a
+          onClick={handleClick}
+          href={`/articles/${article.id}`}
+          className={`${styles.link} p-2 border border-gray-300 shadow-lg hover:shadow-xl transition-shadow duration-200 transform hover:-translate-y-1`}
+        >
+          <picture>
+            {isThumbnailAvailable && (
+              <>
+                <source type="image/webp" media="(max-width: 640px)" srcSet={mobileSrcSet} />
+                <source type="image/webp" srcSet={desktopSrcSet} />
+              </>
+            )}
+            <Image
+              src={imageSrc}
+              alt="サムネイル"
+              className={styles.image}
+              width="600"
+              height="300"
+              placeholder="blur"
+              blurDataURL={imageSrc}
+            />
+          </picture>
+          <div className={styles.content}>
+            <div className={styles.title}>{article.title}</div>
+            <div className={styles.description}>{article.description}</div>
+            <div className={styles.date}>
+              <FolderIcon className="h-5 w-5 mr-2 mt-4" aria-hidden="true" />
+              <TagList tags={article.tags} hasLink={false} />
+              &nbsp;&nbsp;&nbsp;&nbsp;
+              <PublishedDate date={article.publishedAt || article.createdAt} />
+            </div>
           </div>
-        </div>
-      </a>
-    </li>
+        </a>
+      </li>
+      <ins
+        className="adsbygoogle"
+        style={{ display: 'block', width: '100%' }}
+        data-ad-format="fluid"
+        data-ad-layout-key="-5k+co+1n-b5+e3"
+        data-ad-client="ca-pub-1705865999592590"
+        data-ad-slot="1678694276"
+        data-full-width-responsive="false"
+      ></ins>
+    </>
   );
 };
 
