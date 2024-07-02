9d296bef02104eae7e6dec982f839d6a78e932b1
Arata <aratadayoon@yahoo.co.jp>
2024-07-02 13:55:04 +0900

    Revert "feat: affiliate lazy"
    This reverts commit 80ce7f9ecb6f6b3b021be82a1de4003436024e0a.

--- components/FixedSidebar/index.tsx
+++ components/FixedSidebar/index.tsx
@@ -11,7 +11,6 @@ import { Article } from '@/libs/microcms';
 import SidebarArticleListItem from '../SidebarArticleListItem';
 import { tags } from '@/Section/Tag';
 import Instagram from '../Instagram/page';
-import LevatechRookie from '../Affiliate/LevatechRookie';
 import {
   MagnifyingGlassIcon,
   BellAlertIcon,
@@ -257,8 +256,29 @@ export default function FixedSidebar({ articles }: Props) {
             )}
           </div>
         </div>
+
         <Instagram />
-        <LevatechRookie />
+
+        <div className="mt-5 flex justify-center border border-gray-300 py-5 px-4">
+          <Image
+            src="http://www.rentracks.jp/adx/p.gifx?idx=0.61256.344874.638.7876&dna=105544"
+            height="1"
+            width="1"
+            alt=""
+          />
+          <a
+            href="http://www.rentracks.jp/adx/r.html?idx=0.61256.344874.638.7876&dna=105544"
+            rel="nofollow noopener"
+            target="_blank"
+          >
+            <Image
+              src="http://www.image-rentracks.com/7876/300_250.jpg"
+              width="400"
+              height="250"
+              alt="Ad"
+            />
+          </a>
+        </div>
       </div>
     </div>
   );
