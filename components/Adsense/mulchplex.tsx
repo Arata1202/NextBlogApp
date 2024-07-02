10f71ca0d97249aeed3f1765aaf4f34b3774a0e2
Arata <aratadayoon@yahoo.co.jp>
2024-07-02 18:43:33 +0900

    Revert "test-add: mulchplex"
    This reverts commit cedcc72b6785501d3abdb8a5e22ca7deeccf09aa.

--- components/Adsense/mulchplex.tsx
+++ components/Adsense/mulchplex.tsx
@@ -1,12 +1,13 @@
 export default function Mulchplex() {
   return (
-    <div style={{ maxWidth: '100%' }}>
+    <div style={{ maxWidth: '100%', overflow: 'hidden' }}>
       <ins
         className="adsbygoogle"
-        style={{ display: 'block' }}
-        data-ad-format="autorelaxed"
+        style={{ display: 'block', width: '100%' }}
         data-ad-client="ca-pub-1705865999592590"
         data-ad-slot="5469260892"
+        data-ad-format="autorelaxed"
+        data-full-width-responsive="false"
       ></ins>
     </div>
   );
