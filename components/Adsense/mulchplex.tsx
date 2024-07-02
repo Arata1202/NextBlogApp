a5800b69265b4753e2f9d2a4869b81d8a291ec6f
Arata <aratadayoon@yahoo.co.jp>
2024-07-02 18:43:14 +0900

    Revert "test-add: ad"
    This reverts commit 520226b16292d466e71f9d61cf429229c8e2e0b5.

--- components/Adsense/mulchplex.tsx
+++ components/Adsense/mulchplex.tsx
@@ -1,13 +1,13 @@
 export default function Mulchplex() {
   return (
-    <div style={{ maxWidth: '100%' }}>
-      <ins
-        className="adsbygoogle"
-        style={{ display: 'block' }}
-        data-ad-format="autorelaxed"
-        data-ad-client="ca-pub-1705865999592590"
-        data-ad-slot="5469260892"
-      ></ins>
-    </div>
+    // <div style={{ maxWidth: '100%' }}>
+    <ins
+      className="adsbygoogle"
+      style={{ display: 'block' }}
+      data-ad-format="autorelaxed"
+      data-ad-client="ca-pub-1705865999592590"
+      data-ad-slot="5469260892"
+    ></ins>
+    // </div>
   );
 }
