a8bb4c977352d49d3fb86cd4a7d81b4aeaf73a59
Arata <aratadayoon@yahoo.co.jp>
2024-07-04 01:48:44 +0900

    Revert "feat: 目次広告半分"
    This reverts commit 8a206667f8082bbe6c170be9704cc3e94567cdb9.

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -311,14 +311,7 @@ export default function Sidebar({ articles, contentBlocks = [] }: Props) {
         {/* <LevatechRookie /> */}
       </div>
       <div className="SidebarTableOfContens mobile">
-        {headings.length > 0 && (
-          <>
-            <TableOfContents headings={headings} />
-            <div className="FirstAd mt-5">
-              <Display slot="9574685533" />
-            </div>
-          </>
-        )}
+        {headings.length > 0 && <TableOfContents headings={headings} />}
       </div>
     </div>
   );
