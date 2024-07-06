8684f3f7504732c83fbf9e01036841e5fb67c26b
Arata <aratadayoon@yahoo.co.jp>
2024-07-06 17:04:53 +0900

    #134 fix: ショップページ修正

--- components/Fixed/shop.tsx
+++ components/Fixed/shop.tsx
@@ -52,6 +52,42 @@ const ShopPage: React.FC<{ sidebarArticles: any }> = ({ sidebarArticles }) => {
       imageSrc: '/images/head/realstudent512.png',
       imageAlt: 'Hand stitched, orange エコバッグ.',
     },
+    {
+      id: 5,
+      name: 'エコバッグ',
+      color: 'Natural',
+      price: '1000円 （税込）',
+      href: '#',
+      imageSrc: '/images/head/realstudent512.png',
+      imageAlt: 'Hand stitched, orange エコバッグ.',
+    },
+    {
+      id: 6,
+      name: 'エコバッグ',
+      color: 'Natural',
+      price: '1000円 （税込）',
+      href: '#',
+      imageSrc: '/images/head/realstudent512.png',
+      imageAlt: 'Hand stitched, orange エコバッグ.',
+    },
+    {
+      id: 7,
+      name: 'エコバッグ',
+      color: 'Natural',
+      price: '1000円 （税込）',
+      href: '#',
+      imageSrc: '/images/head/realstudent512.png',
+      imageAlt: 'Hand stitched, orange エコバッグ.',
+    },
+    {
+      id: 8,
+      name: 'エコバッグ',
+      color: 'Natural',
+      price: '1000円 （税込）',
+      href: '#',
+      imageSrc: '/images/head/realstudent512.png',
+      imageAlt: 'Hand stitched, orange エコバッグ.',
+    },
   ];
 
   return (
@@ -72,21 +108,31 @@ const ShopPage: React.FC<{ sidebarArticles: any }> = ({ sidebarArticles }) => {
                 <AdAlert />
                 <div className="mt-10 mb-5 grid grid-cols-2 gap-x-4 gap-y-10 sm:gap-x-6 md:grid-cols-4 md:gap-y-0 lg:gap-x-8">
                   {products.map((product) => (
-                    <div key={product.id} className="group relative">
-                      <div className="w-full bg-gray-200">
-                        <img
-                          alt={product.imageAlt}
-                          src={product.imageSrc}
-                          className="h-full w-full object-cover object-center"
-                        />
+                    <div key={product.id} className="mt-10 mb-5">
+                      <div className="group relative">
+                        <div className="w-full bg-gray-200">
+                          <img
+                            alt={product.imageAlt}
+                            src={product.imageSrc}
+                            className="h-full w-full object-cover object-center"
+                          />
+                        </div>
+                        <div className="mt-2 text-lg font-bold text-gray-700">
+                          <>
+                            <span className="absolute inset-0" />
+                            {product.name}
+                          </>
+                        </div>
+                        <div className="mt-1 text-lg text-gray-900">{product.price}</div>
                       </div>
-                      <div className="mt-2 text-lg font-bold text-gray-700">
-                        <a href={product.href}>
-                          <span className="absolute inset-0" />
-                          {product.name}
+                      <div className="mt-2">
+                        <a
+                          href={product.href}
+                          className="block w-full rounded-md bg-white px-3.5 py-1.5 text-center text-sm font-semibold text-gray-800 shadow-s border border-gray-300"
+                        >
+                          購入する
                         </a>
                       </div>
-                      <div className="mb-2 mt-1 text-lg text-gray-900">{product.price}</div>
                     </div>
                   ))}
                 </div>
