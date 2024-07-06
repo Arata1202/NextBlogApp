943f48ec6bd73be7790b2d7968b5d8a546b8a405
Arata <aratadayoon@yahoo.co.jp>
2024-07-06 15:37:23 +0900

    fix: 最新記事　ページネーション

--- app/p/[current]/page.tsx
+++ app/p/[current]/page.tsx
@@ -31,8 +31,8 @@ export default async function Page({ params }: Props) {
   });
   return (
     <>
-      <h1 className="categoryTitle text-3xl font-bold pt-5 max-w-[85rem] sm:px-6 lg:px-8 mx-auto pb-2">
-        <div className="flex items-center pb-2 pt-2">
+      <h1 className="categoryTitle text-3xl font-bold pt-5 pb-2 max-w-[85rem] sm:px-6 lg:px-8 mx-auto">
+        <div className="topTitle flex items-center pb-2 pt-2 mt-10">
           <BellAlertIcon className="h-8 w-8 mr-2" aria-hidden="true" />
           <div>最新記事</div>
         </div>
