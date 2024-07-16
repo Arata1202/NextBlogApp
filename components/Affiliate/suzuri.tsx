bb12132cae04080be6f1a0d22c68e7f1bda21cac
Arata <aratadayoon@yahoo.co.jp>
2024-07-17 00:11:36 +0900

    feat: SuzuriBanner

---
+++ components/Affiliate/suzuri.tsx
@@ -0,0 +1,16 @@
+export default function Suzuri() {
+  return (
+    <>
+      <p className="mt-5 text-center">\ オリジナルグッズ販売中 /</p>
+      <div className="flex justify-center">
+        <a
+          href="https://suzuri.jp/realunivlog"
+          target="brank"
+          className="duration-200 transform hover:-translate-y-1"
+        >
+          <img width="400" height="250" src="/images/shop/suzuri.png" alt="suzuri" />
+        </a>
+      </div>
+    </>
+  );
+}
