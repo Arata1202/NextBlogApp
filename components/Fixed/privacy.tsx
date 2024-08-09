5e655e3939bf0dfe71b8031e7e884c4bf99670fa
Arata <aratadayoon@yahoo.co.jp>
2024-08-09 12:30:41 +0900

    fix: プライバシー改定

--- components/Fixed/privacy.tsx
+++ components/Fixed/privacy.tsx
@@ -4,7 +4,6 @@ import styles from './index.module.css';
 import PublishedDate from '@/components/Date';
 import React from 'react';
 import { InformationCircleIcon, HomeIcon, ChevronRightIcon } from '@heroicons/react/24/solid';
-import Link from 'next/link';
 import FixedSidebar from '@/components/FixedSidebar';
 import Share from '../Share';
 import AdAlert from '../AdAlert';
@@ -20,7 +19,7 @@ const PrivacyPage: React.FC<{ sidebarArticles: any }> = ({ sidebarArticles }) =>
   //   { id: 'analytics', title: 'アクセス解析ツール', level: 3 },
   //   { id: 'comment', title: 'コメントについて', level: 3 },
   //   { id: 'purpose', title: '個人情報の利用目的', level: 2 },
-  //   { id: 'advertisement', title: '当サイトが利用している広告サービス', level: 2 },
+  //   { id: 'advertisement', title: '本サービスが利用している広告サービス', level: 2 },
   //   { id: 'amazon', title: 'Amazonアソシエイトプログラム', level: 3 },
   //   { id: 'google', title: 'Googleアドセンス', level: 3 },
   //   { id: 'management', title: '個人情報の管理方法', level: 2 },
@@ -61,7 +60,7 @@ const PrivacyPage: React.FC<{ sidebarArticles: any }> = ({ sidebarArticles }) =>
               <div className={`${styles.content} mt-10 mb-5`}>
                 <h2 id="introduction">個人情報取り扱いに関する基本方針</h2>
                 <p>
-                  リアル大学生（以下、「当サイト」と言います。）では、ご利用頂くお客様の個人情報を適切に保護するため、個人情報の保護に関する法律、その他の関係法令を遵守すると共に、以下に定めるプライバシーポリシーに従って、個人情報を安全かつ適切に取り扱うことを宣言いたします。
+                  リアル大学生（以下、「本サービス」と言います。）では、ご利用頂くお客様の個人情報を適切に保護するため、個人情報の保護に関する法律、その他の関係法令を遵守すると共に、以下に定めるプライバシーポリシーに従って、個人情報を安全かつ適切に取り扱うことを宣言いたします。
                 </p>
                 <h2 id="definition">個人情報の定義</h2>
                 <p>
@@ -69,32 +68,32 @@ const PrivacyPage: React.FC<{ sidebarArticles: any }> = ({ sidebarArticles }) =>
                 </p>
                 <h2 id="acquisition">個人情報の取得方法</h2>
                 <p>
-                  当サイトはユーザーが利用登録をする際に氏名、住所、電話番号、メールアドレス、などの個人情報をお尋ねすることがあります。また、ユーザーと提携先などとの間でなされたユーザーの個人情報を含む取引記録や決済に関する情報を、当サイトの提携先（情報提供元、広告主、広告配信先などを含みます。以下、｢提携先｣といいます。）などから収集することがあります。
+                  本サービスはユーザーが利用登録をする際に氏名、住所、電話番号、メールアドレス、などの個人情報をお尋ねすることがあります。また、ユーザーと提携先などとの間でなされたユーザーの個人情報を含む取引記録や決済に関する情報を、本サービスの提携先（情報提供元、広告主、広告配信先などを含みます。以下、｢提携先｣といいます。）などから収集することがあります。
                 </p>
                 <h3 id="cookie">クッキー（Cookie）</h3>
                 <p>
-                  当サイトでは、アクセス解析サービス、各種アフィリエイトプログラム、広告配信サービスを利用しております。これらの広告配信業者は、ユーザーのご興味に応じた商品やサービスの広告を表示するため、ユーザーの当サイトおよび他サイトへのアクセスに関する情報Cookieを使用することがございます。
+                  本サービスでは、アクセス解析サービス、各種アフィリエイトプログラム、広告配信サービスを利用しております。これらの広告配信業者は、ユーザーのご興味に応じた商品やサービスの広告を表示するため、ユーザーの本サービスおよび他サイトへのアクセスに関する情報Cookieを使用することがございます。
                   <br />
                   <br />
-                  Cookieは、ユーザーが当サイトあるいは他サイトを閲覧された際、使用されたコンピューターやデバイス内に記録されます。ただし、この情報には、お名前・ご住所・メールアドレス・電話番号など個人を特定できるものは一切含まれません。
+                  Cookieは、ユーザーが本サービスあるいは他サイトを閲覧された際、使用されたコンピューターやデバイス内に記録されます。ただし、この情報には、お名前・ご住所・メールアドレス・電話番号など個人を特定できるものは一切含まれません。
                   <br />
                   <br />
                   Cookieによる情報収集を好まれない場合、ユーザーご自身でブラウザで受け入れを拒否するよう設定することも可能です。ただし、この設定により一部のコンテンツが正しく機能しない場合、またサービスが受けられない場合がございます。あらかじめご了承ください。
                   <br />
                   <br />
                   なお、設定方法に関しては
-                  <Link
+                  <a
                     href="https://policies.google.com/technologies/partner-sites?hl=ja"
                     className="text-blue-500 hover:text-blue-700"
                     target="blank"
                   >
                     Googleポリシーと規約
-                  </Link>
+                  </a>
                   にてご確認いただけます。
                 </p>
                 <h3 id="analytics">アクセス解析ツール</h3>
                 <p>
-                  当サイトでは、Googleの提供するアクセス解析ツール『Google
+                  本サービスでは、Googleの提供するアクセス解析ツール『Google
                   Analytics』を使用しています。Google
                   Analyticsはトラフィックデータの収集のためCookieを利用します。このトラフィックデータは匿名で収集されており、個人を特定するものではございません。
                   <br />
@@ -103,21 +102,21 @@ const PrivacyPage: React.FC<{ sidebarArticles: any }> = ({ sidebarArticles }) =>
                   <br />
                   <br />
                   なお、この規約に関しては、
-                  <Link
+                  <a
                     href="https://marketingplatform.google.com/about/analytics/terms/jp/"
                     className="text-blue-500 hover:text-blue-700"
                     target="blank"
                   >
                     Googleアナリティクス利用規約
-                  </Link>
+                  </a>
                   および
-                  <Link
+                  <a
                     href="https://policies.google.com/technologies/partner-sites?hl=ja"
                     className="text-blue-500 hover:text-blue-700"
                     target="blank"
                   >
                     Googleポリシーと規約
-                  </Link>
+                  </a>
                   でご確認いただけます。
                 </p>
                 <h3 id="comment">コメントについて</h3>
@@ -132,23 +131,23 @@ const PrivacyPage: React.FC<{ sidebarArticles: any }> = ({ sidebarArticles }) =>
                 </p>
                 <h2 id="purpose">個人情報の利用目的</h2>
                 <p>
-                  当サイトが個人情報を収集・利用する目的は、以下のとおりです。
+                  本サービスが個人情報を収集・利用する目的は、以下のとおりです。
                   <br />
                   <br />
-                  当サイトのお問い合わせ際に、名前（ニックネーム）、メールアドレス等の個人情報を入力いただく場合がございます。
+                  本サービスのお問い合わせ際に、名前（ニックネーム）、メールアドレス等の個人情報を入力いただく場合がございます。
                   <br />
                   <br />
                   これらの個人情報は、質問に対する回答や必要な情報を電子メールなどでご連絡する場合に利用させていただくものであり、個人情報をご提供いただく際の目的以外では利用いたしません。
                   <br />
                   <br />
-                  当サイトでは，有料のサービスを提供しております。その際の利用料金のご請求をするために利用しております。
+                  本サービスでは，有料のサービスを提供しております。その際の利用料金のご請求をするために利用しております。
                   <br />
                   <br />
-                  その他に、当サイトを不正・不当な目的で利用しようとするユーザーがいた場合、ユーザーの特定をし、ご利用をお断りするために利用します。
+                  その他に、本サービスを不正・不当な目的で利用しようとするユーザーがいた場合、ユーザーの特定をし、ご利用をお断りするために利用します。
                 </p>
-                <h2 id="advertisement">当サイトが利用している広告サービス</h2>
+                <h2 id="advertisement">本サービスが利用している広告サービス</h2>
                 <p>
-                  当サイトは、以下の第三者の配信する広告サービスならびにアフィリエイトプログラムを利用しております。
+                  本サービスは、以下の第三者の配信する広告サービスならびにアフィリエイトプログラムを利用しております。
                 </p>
                 <ul>
                   <li>もしもアフィリエイト</li>
@@ -159,29 +158,31 @@ const PrivacyPage: React.FC<{ sidebarArticles: any }> = ({ sidebarArticles }) =>
                 </p>
                 <h3 id="amazon">Amazonアソシエイトプログラム</h3>
                 <p>Amazonのアソシエイトとして、当メディアは適格販売により収入を得ています。</p>
-                <h3 id="google">Googleアドセンス</h3>
+                <h3 id="google">Googleの広告サービス</h3>
+                <p>本サービスは、Googleの提供する以下の広告サービスを利用しています。</p>
+                <ul>
+                  <li>Google AdSense</li>
+                  <li>Google AdMob</li>
+                </ul>
                 <p>
-                  当サイトは、第三者配信の広告サービス「Google
-                  Adsense（グーグルアドセンス）」を利用しています。当サイトにおいて広告が配信される過程で、Cookieやデバイス特有の情報、位置情報、当該デバイスから収集されるその他の情報が利用される場合があります。ただしその過程で個人を特定できる情報は収集されません。
+                  これらの広告サービスにおいて、Cookieやデバイス特有の情報、位置情報、当該デバイスから収集されるその他の情報が利用される場合があります。ただし、これらの情報で個人を特定することはありません。
                   <br />
                   <br />
-                  また、Google
-                  AdSenseに関して、このプロセスの詳細や情報が広告配信事業者に使用されないよう、ユーザーご自身で設定することができます。ただし、この設定により一部のコンテンツが正しく機能しない場合、またサービスが受けられない場合がございます。あらかじめご了承ください。
+                  ユーザーは、広告設定に関してGoogleのポリシーと規約に従い、設定を変更することができます。ただし、この設定により一部のコンテンツが正しく機能しない場合があります。あらかじめご了承ください。
                   <br />
                   <br />
-                  なお、この設定方法に関しては
-                  <Link
+                  詳細については、以下のリンクをご参照ください：
+                  <a
                     href="https://policies.google.com/technologies/partner-sites?hl=ja"
                     className="text-blue-500 hover:text-blue-700"
                     target="blank"
                   >
                     Googleポリシーと規約
-                  </Link>
-                  にてご確認いただけます。
+                  </a>
                 </p>
                 <h2 id="management">個人情報の管理方法</h2>
                 <p>
-                  当サイトは第三者に皆さまの重要な情報を読み取られたり、改ざんされたりすることを防ぐために、SSLを使用しております。
+                  本サービスは第三者に皆さまの重要な情報を読み取られたり、改ざんされたりすることを防ぐために、SSLを使用しております。
                   <br />
                   <br />
                   SSL(SecureSocketLayer)とはデータを暗号化して通信するセキュリティ機能です。
@@ -189,7 +190,7 @@ const PrivacyPage: React.FC<{ sidebarArticles: any }> = ({ sidebarArticles }) =>
                 </p>
                 <h2 id="third-party">個人情報の第三者提供</h2>
                 <p>
-                  当サイトは次に掲げる場合を除いて、あらかじめユーザーの同意を得ることなく第三者に個人情報を提供することはありません。
+                  本サービスは次に掲げる場合を除いて、あらかじめユーザーの同意を得ることなく第三者に個人情報を提供することはありません。
                   <br />
                   <br />
                   ただし、個人情報保護法その他の法令で認められる場合を除きます。
@@ -206,9 +207,9 @@ const PrivacyPage: React.FC<{ sidebarArticles: any }> = ({ sidebarArticles }) =>
                   サイト運営者：あお
                   <br />
                   連絡先：
-                  <Link href="/contact" className="text-blue-500 hover:text-blue-700">
+                  <a href="/contact" className="text-blue-500 hover:text-blue-700">
                     https://realunivlog.com/contact/
-                  </Link>
+                  </a>
                 </p>
               </div>
             </div>
