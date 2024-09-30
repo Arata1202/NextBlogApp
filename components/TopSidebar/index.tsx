7b6a2b6bff63349b08aa7005934c72dd24bce2f0
Arata <aratadayoon@yahoo.co.jp>
2024-09-30 14:23:30 +0900

    add: news

--- components/TopSidebar/index.tsx
+++ components/TopSidebar/index.tsx
@@ -6,8 +6,9 @@ import Image from 'next/image';
 import styles from './index.module.css';
 import { UserProfile } from '@/section/Dummy';
 import { SocialIcon, CategoryList, CategoryList2, PopularPost } from '@/section/Dummy';
+import { NewspaperIcon } from '@heroicons/react/24/solid';
 import Display from '../Adsense/display';
-import Suzuri from '../Affiliate/suzuri';
+import { news } from '@/section/news';
 // import { OneSignalInitial } from '@/libs/OneSignalInitial';
 import {
   MagnifyingGlassIcon,
@@ -101,7 +102,51 @@ export default function TopSidebar() {
           </div>
         </div>
 
-        <Suzuri />
+        <div className="bg-white pt-8 px-4 border border-gray-300 py-5 mt-5">
+          <h1
+            className={`${styles.profile} text-2xl text-center font-semibold flex justify-center`}
+          >
+            <NewspaperIcon className="h-8 w-8 mr-2" aria-hidden="true" />
+            お知らせ
+          </h1>
+
+          <div className="">
+            <div className="mt-5" style={{ maxHeight: '240px', overflowY: 'auto' }}>
+              <table className="min-w-full">
+                <colgroup>
+                  <col className="w-full sm:w-1/2" />
+                </colgroup>
+                <tbody>
+                  {news.map((news, index) => (
+                    <tr key={news.id} className="border-b border-gray-200">
+                      <td
+                        className={`max-w-0 ${
+                          index === 0 ? 'pt-0 pb-5' : 'py-5'
+                        } pl-4 pr-3 text-sm sm:pl-0`}
+                      >
+                        <div style={{ fontSize: '18px' }} className="text-gray-900">
+                          {news.name}
+                        </div>
+                        <div className="mt-1 truncate">
+                          <a
+                            className="text-blue-500 hover:text-blue-700"
+                            target="blank"
+                            href={news.url}
+                          >
+                            {news.meta}
+                          </a>
+                        </div>
+                        <div className="mt-1 truncate text-gray-500">{news.description}</div>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+
+        {/* <Suzuri /> */}
 
         <div className="bg-white pt-8 px-4 border border-gray-300 py-5 mt-5">
           <h1
---
+++ section/news.tsx
@@ -0,0 +1,23 @@
+export const news = [
+  {
+    id: 3,
+    name: 'Android版アプリ「リアル大学生：モバイル」がリリースされました。',
+    description: '2024年9月30日',
+    meta: 'Google Playからダウンロードする',
+    url: 'https://play.google.com/store/apps/details?id=com.realunivlog.flutterblogapp',
+  },
+  {
+    id: 2,
+    name: 'RSSの不具合を修正しました。お手数ですが、新しいURLから再登録をお願いします。',
+    description: '2024年9月30日',
+    meta: 'RSSを登録する',
+    url: 'https://meta.realunivlog.com/rss.xml',
+  },
+  {
+    id: 1,
+    name: 'iOS版アプリ「リアル大学生：モバイル」がリリースされました。',
+    description: '2024年8月3日',
+    meta: 'App Storeからダウンロードする',
+    url: 'https://apps.apple.com/jp/app/リアル大学生-モバイル/id6590619103',
+  },
+];
