2f4faec929bbbe86f32cf5603397321ecc4ee4da
Arata <aratadayoon@yahoo.co.jp>
2024-10-02 15:36:35 +0900

    #176 add: archive_data

---
+++ section/archive.tsx
@@ -0,0 +1,11 @@
+export const archive = [
+  { year: '2024', month: '10', monthForPath: '10' },
+  { year: '2024', month: '9', monthForPath: '09' },
+  { year: '2024', month: '8', monthForPath: '08' },
+  { year: '2024', month: '7', monthForPath: '07' },
+  { year: '2024', month: '6', monthForPath: '06' },
+  { year: '2024', month: '5', monthForPath: '05' },
+  { year: '2024', month: '2', monthForPath: '02' },
+  { year: '2024', month: '1', monthForPath: '01' },
+  { year: '2023', month: '12', monthForPath: '12' },
+];
