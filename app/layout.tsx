477d4b86513e312072f0ed38eee512d76dc0e0f4
Arata <aratadayoon@yahoo.co.jp>
2024-10-02 16:43:25 +0900

    #177 fix: header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from '@/components/Header';
+import Header from '@/components/Layouts/Header';
 import Footer from '@/components/Footer';
 import './globals.css';
 import styles from './layout.module.css';
