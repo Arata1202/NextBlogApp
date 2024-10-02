4a6d8b540eb6bd7a9dde00a66cc4d145d4a9f192
Arata <aratadayoon@yahoo.co.jp>
2024-10-02 21:52:32 +0900

    fix: アーカイブの位置

--- components/Sidebars/TopSidebar/index.tsx
+++ components/Sidebars/TopSidebar/index.tsx
@@ -248,46 +248,6 @@ export default function TopSidebar() {
           <Display slot="8452341403" />
         </div>
 
-        <div className="sidebarArticleList">
-          <div className="bg-white pt-8 px-4 border border-gray-300 py-5 mt-5">
-            <h1
-              className={`${styles.profile} text-2xl text-center font-semibold flex justify-center`}
-            >
-              <BoltIcon className="h-8 w-8 mr-2" aria-hidden="true" />
-              人気の投稿
-            </h1>
-            {PopularPost.map((item, index) => (
-              <ol
-                key={item.postName}
-                className="ArticleListItem_list border mt-5 border-gray-300 p-2 shadow-lg hover:shadow-xl transition-shadow duration-200 transform hover:-translate-y-1"
-              >
-                <li>
-                  <a
-                    href={item.postHref}
-                    className="sidebarPopularPost"
-                    onClick={(event: React.MouseEvent<HTMLAnchorElement>) => {
-                      event.preventDefault();
-                      window.location.href = item.postHref;
-                    }}
-                  >
-                    <Image
-                      src={item.imageHref}
-                      alt={item.imageAlt}
-                      className="ArticleListItem_image"
-                      width="600"
-                      height="300"
-                      key={index}
-                    />
-                    <div>
-                      <div className="ArticleListItem_title font-bold">{item.postName}</div>
-                    </div>
-                  </a>
-                </li>
-              </ol>
-            ))}
-          </div>
-        </div>
-
         <div className="bg-white pt-8 px-4 border border-gray-300 py-5 mt-5">
           <h1
             className={`${styles.profile} text-2xl text-center font-semibold flex justify-center`}
@@ -337,6 +297,46 @@ export default function TopSidebar() {
               </Listbox.Options>
             </div>
           </Listbox>
+        </div>
+
+        <div className="sidebarArticleList">
+          <div className="bg-white pt-8 px-4 border border-gray-300 py-5 mt-5">
+            <h1
+              className={`${styles.profile} text-2xl text-center font-semibold flex justify-center`}
+            >
+              <BoltIcon className="h-8 w-8 mr-2" aria-hidden="true" />
+              人気の投稿
+            </h1>
+            {PopularPost.map((item, index) => (
+              <ol
+                key={item.postName}
+                className="ArticleListItem_list border mt-5 border-gray-300 p-2 shadow-lg hover:shadow-xl transition-shadow duration-200 transform hover:-translate-y-1"
+              >
+                <li>
+                  <a
+                    href={item.postHref}
+                    className="sidebarPopularPost"
+                    onClick={(event: React.MouseEvent<HTMLAnchorElement>) => {
+                      event.preventDefault();
+                      window.location.href = item.postHref;
+                    }}
+                  >
+                    <Image
+                      src={item.imageHref}
+                      alt={item.imageAlt}
+                      className="ArticleListItem_image"
+                      width="600"
+                      height="300"
+                      key={index}
+                    />
+                    <div>
+                      <div className="ArticleListItem_title font-bold">{item.postName}</div>
+                    </div>
+                  </a>
+                </li>
+              </ol>
+            ))}
+          </div>
         </div>
       </div>
     </div>
