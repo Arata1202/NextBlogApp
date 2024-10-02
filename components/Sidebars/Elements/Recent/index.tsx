68d17d94c99c20fffd4a9d76bbd9369c19cd67c9
Arata <aratadayoon@yahoo.co.jp>
2024-10-03 08:36:30 +0900

    fix: FixedSidebar

---
+++ components/Sidebars/Elements/Recent/index.tsx
@@ -0,0 +1,41 @@
+// Recent/index.tsx
+'use client';
+
+import React from 'react';
+import SidebarArticleListItem from '../../SidebarArticleListItem';
+import { Article } from '@/libs/microcms';
+import { BellAlertIcon } from '@heroicons/react/24/solid';
+import styles from '../../FixedSidebar/index.module.css';
+
+type Props = {
+  articles?: Article[];
+};
+
+export default function Recent({ articles }: Props) {
+  const sortedArticles = articles
+    ?.slice()
+    .sort((a, b) => {
+      const dateA = a.publishedAt ? new Date(a.publishedAt).getTime() : 0;
+      const dateB = b.publishedAt ? new Date(b.publishedAt).getTime() : 0;
+      return dateB - dateA;
+    })
+    .slice(0, 3);
+
+  return (
+    <div className="bg-white pt-8 px-4 border border-gray-300 py-5 mt-5">
+      <h1 className={`${styles.profile} text-2xl text-center font-semibold flex justify-center`}>
+        <BellAlertIcon className="h-8 w-8 mr-2" aria-hidden="true" />
+        最新の投稿
+      </h1>
+      {sortedArticles && sortedArticles.length > 0 ? (
+        <div>
+          {sortedArticles.map((article) => (
+            <SidebarArticleListItem key={article.id} article={article} />
+          ))}
+        </div>
+      ) : (
+        <p className="text-center">新着記事はありません</p>
+      )}
+    </div>
+  );
+}
---
+++ components/Sidebars/Elements/Search/index.tsx
@@ -0,0 +1,17 @@
+import { MagnifyingGlassIcon } from '@heroicons/react/24/solid';
+import SearchField from '../../../Elements/SearchField';
+import styles from '../../FixedSidebar/index.module.css';
+
+export default function Search() {
+  return (
+    <div className="bg-white pt-8 px-4 border border-gray-300 py-5">
+      <h1
+        className={`${styles.profile} text-2xl text-center font-semibold mb-5 flex justify-center`}
+      >
+        <MagnifyingGlassIcon className="h-8 w-8 mr-2" aria-hidden="true" />
+        キーワードで探す
+      </h1>
+      <SearchField />
+    </div>
+  );
+}
