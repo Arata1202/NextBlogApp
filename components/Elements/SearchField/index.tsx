d2641b7ffe6d9db5e5f3a246001c2e9b5b6e9545
Arata <aratadayoon@yahoo.co.jp>
2024-10-16 11:32:46 +0900

    Revert "#181 feat: SearchHistory"
    This reverts commit 9b7896db117ddc720a4636be8b981b3d7cfb021c.

--- components/Elements/SearchField/index.tsx
+++ components/Elements/SearchField/index.tsx
@@ -1,52 +1,18 @@
-import React, { useState, FormEvent, useRef } from 'react';
+//最適化済み
+
+import React from 'react';
 import styles from './index.module.css';
 
-interface SearchFieldProps {
-  defaultQuery?: string;
-}
-
-export default function SearchField({ defaultQuery = '' }: SearchFieldProps) {
-  const [query, setQuery] = useState<string>(defaultQuery);
-  const formRef = useRef<HTMLFormElement | null>(null);
-
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    try {
-      const res = await fetch(
-        'https://7wr3dauqaae63rgh56bjei6eea0pfcpj.lambda-url.ap-northeast-2.on.aws/',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ query }),
-        },
-      );
-
-      if (res.ok) {
-        console.log('Query saved successfully');
-        if (formRef.current) {
-          formRef.current.submit();
-        }
-      } else {
-        console.error('Failed to save query');
-      }
-    } catch (error) {
-      console.error('Error saving query:', error);
-    }
-  };
-
+export default function SearchField({ defaultQuery = '' }) {
   return (
-    <form ref={formRef} action="/search" method="GET" onSubmit={handleSubmit}>
+    <form action="/search" method="GET">
       <input
         type="search"
         name="q"
         className={`${styles.search} hover:border-blue-500`}
         placeholder="検索"
-        defaultValue={query}
+        defaultValue={defaultQuery}
         style={{ width: '100%', borderRadius: '0' }}
-        onChange={(e) => setQuery(e.target.value)}
       />
       <button type="submit" style={{ display: 'none' }}>
         Search
