6f5aeea09dc0fab96dea736ba2c51b618deed2c2
Arata <aratadayoon@yahoo.co.jp>
2024-10-26 13:45:52 +0900

    #184 chore: Jest

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.+)$': '<rootDir>/$1',
+  },
+};
+
+export default config;
