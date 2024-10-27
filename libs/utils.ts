fc480b955d9ec820f52b5d147abf493b7987d1b0
Arata <aratadayoon@yahoo.co.jp>
2024-10-27 18:29:41 +0900

    #188 fix: Component

--- libs/utils.ts
+++ libs/utils.ts
@@ -1,12 +1,12 @@
 import { format } from 'date-fns';
-import { utcToZonedTime } from 'date-fns-tz';
+import { toZonedTime } from 'date-fns-tz';
 import cheerio from 'cheerio';
 import hljs from 'highlight.js';
 import 'highlight.js/styles/hybrid.css';
 
 export const formatDate = (date: string) => {
   const utcDate = new Date(date);
-  const jstDate = utcToZonedTime(utcDate, 'Asia/Tokyo');
+  const jstDate = toZonedTime(utcDate, 'Asia/Tokyo');
   return format(jstDate, 'yyyy年M月d日');
 };
 
