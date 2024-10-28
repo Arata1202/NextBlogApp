55153de2bfb2a85da398999a0b109a661782e5ab
Arata <aratadayoon@yahoo.co.jp>
2024-10-29 00:00:55 +0900

    fix: PWA廃止

--- next.config.ts
+++ next.config.ts
@@ -26,10 +26,11 @@ const nextConfig: NextConfig = {
   },
 };
 
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-});
+// const withPWA = require('next-pwa')({
+//   dest: 'public',
+//   register: true,
+//   skipWaiting: true,
+// });
 
-export default withPWA(nextConfig);
+// export default withPWA(nextConfig);
+export default nextConfig;
