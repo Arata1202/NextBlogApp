2253c37590a1bc83c05f7352b1ddc6e699b383a6
Arata <aratadayoon@yahoo.co.jp>
2024-10-30 16:18:38 +0900

    #194 add: Jest ArticleLists

---
+++ test/components/ArticleLists/ArticleListItem.test.tsx
@@ -0,0 +1,22 @@
+import { render } from '@testing-library/react';
+import ArticleListItem from '@/components/ArticleLists/ArticleListItem';
+import { Article } from '@/libs/microcms';
+
+const article: Article = {
+  id: '1',
+  title: 'タイトル',
+  description: '概要',
+  thumbnail: { url: 'https://thumbnail.com/thumbnail.jpg' },
+  publishedAt: '2023-10-30T12:00:00Z',
+  content_blocks: [],
+  introduction_blocks: [],
+  createdAt: '2023-10-30T12:00:00Z',
+  updatedAt: '2023-10-30T12:00:00Z',
+};
+
+describe('ArticleListItem', () => {
+  test('ArticleListItem)', () => {
+    const { asFragment } = render(<ArticleListItem key={article.id} article={article} />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
