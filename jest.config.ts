c1413e09e7e65fe49206766e40c08029b7fc2d87
Arata <aratadayoon@yahoo.co.jp>
2024-10-30 15:25:40 +0900

    #194 add: Jest tsconfig.test

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,11 @@ import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.test.json',
+    },
+  },
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
