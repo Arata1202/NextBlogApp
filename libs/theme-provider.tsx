75bf225165c0b4cdb4bf5f52dfd13a92c73f5314
Arata <aratadayoon@yahoo.co.jp>
2024-11-06 19:40:00 +0900

    #203 fix: provider

--- libs/theme-provider.tsx
+++ libs/theme-provider.tsx
@@ -2,8 +2,18 @@
 
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import type { ThemeProviderProps } from 'next-themes';
-import { FC } from 'react';
+import { FC, useState, useEffect } from 'react';
 
 export const ThemeProvider: FC<ThemeProviderProps> = (props) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
   return <NextThemesProvider {...props}>{props.children}</NextThemesProvider>;
 };
