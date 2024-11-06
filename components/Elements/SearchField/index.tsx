f741f8f947331f48adb6639eea52c21dc1752af6
Arata <aratadayoon@yahoo.co.jp>
2024-11-06 19:52:57 +0900

    #203 dark: search

--- components/Elements/SearchField/index.tsx
+++ components/Elements/SearchField/index.tsx
@@ -1,13 +1,16 @@
+'use client';
+import { useTheme } from 'next-themes';
 import React from 'react';
 import styles from './index.module.css';
 
 export default function SearchField({ defaultQuery = '' }) {
+  const { theme } = useTheme();
   return (
     <form action="/search" method="GET">
       <input
         type="search"
         name="q"
-        className={`${styles.search} hover:border-blue-500`}
+        className={`${styles.search} hover:border-blue-500 border ${theme === 'dark' ? 'DarkTheme placeholder:text-white' : 'LightTheme placeholder:text-gray-700'}`}
         placeholder="検索"
         defaultValue={defaultQuery}
         style={{ width: '100%', borderRadius: '0' }}
