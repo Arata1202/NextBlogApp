c88968d201b774de64a194bf07b530b46f0ec392
Arata <aratadayoon@yahoo.co.jp>
2024-12-26 21:55:27 +0900

    refactor:

--- next.config.ts
+++ next.config.ts
@@ -26,11 +26,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-// const withPWA = require('next-pwa')({
-//   dest: 'public',
-//   register: true,
-//   skipWaiting: true,
-// });
-
-// export default withPWA(nextConfig);
 export default nextConfig;
