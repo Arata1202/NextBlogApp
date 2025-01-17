fa2175e15ee61042d9c8e107cbc92349be2b6bd5
Arata <aratadayoon@yahoo.co.jp>
2025-01-18 00:29:04 +0900

    test: Ad あと少し

--- hooks/MutationObserver/index.tsx
+++ hooks/MutationObserver/index.tsx
@@ -2,31 +2,25 @@ import { useEffect } from 'react';
 
 export const useGuardObserver = () => {
   useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      const targets = document.querySelectorAll<HTMLElement>('.mut-guard');
-      const observers: MutationObserver[] = [];
+    const targets = document.querySelectorAll<HTMLElement>('.mut-guard');
+    const observers: MutationObserver[] = [];
 
-      targets.forEach((target) => {
-        const heightChangeObserver = new MutationObserver(() => {
-          target.style.height = '';
-          target.style.minHeight = '';
-        });
-
-        heightChangeObserver.observe(target, {
-          attributes: true,
-          attributeFilter: ['style'],
-        });
-
-        observers.push(heightChangeObserver);
+    targets.forEach((target) => {
+      const heightChangeObserver = new MutationObserver(() => {
+        target.style.height = '';
+        target.style.minHeight = '';
       });
 
-      return () => {
-        observers.forEach((observer) => observer.disconnect());
-      };
-    }, 100);
+      heightChangeObserver.observe(target, {
+        attributes: true,
+        attributeFilter: ['style'],
+      });
+
+      observers.push(heightChangeObserver);
+    });
 
     return () => {
-      clearTimeout(timeoutId);
+      observers.forEach((observer) => observer.disconnect());
     };
   }, []);
 };
