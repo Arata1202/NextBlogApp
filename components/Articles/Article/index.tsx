1e5219fb97f05ee6a74b6b63501dd6ae6e4e56cb
Arata <aratadayoon@yahoo.co.jp>
2025-02-06 23:05:35 +0900

    fix: 外部リンク

--- components/Articles/Article/index.tsx
+++ components/Articles/Article/index.tsx
@@ -49,7 +49,6 @@ import {
   HandThumbUpIcon,
   HandThumbDownIcon,
   LinkIcon,
-  ArrowTopRightOnSquareIcon,
   LightBulbIcon,
   InformationCircleIcon,
   HomeIcon,
@@ -298,15 +297,7 @@ export default function Article({ data, articles }: Props) {
                         <WithArticleItem article={block.articleLink3 as ArticleType} />
                       </div>
                     )}
-                    {block.external_link && (
-                      <div>
-                        <div className="flex mt-10">
-                          <ArrowTopRightOnSquareIcon className="h-8 w-8 mr-2" aria-hidden="true" />
-                          <h1 className="text-2xl font-semibold mb-5">外部リンク</h1>
-                        </div>
-                        <ExternalLinkItem url={block.external_link} />
-                      </div>
-                    )}
+                    {block.external_link && <ExternalLinkItem url={block.external_link} />}
                     {block.box_merit && (
                       <div className={`${styles.tab_merit_box} text-gray-700 flex items-center`}>
                         <HandThumbUpIcon
@@ -408,18 +399,7 @@ export default function Article({ data, articles }: Props) {
                           <WithArticleItem article={block.articleLink3 as ArticleType} />
                         </div>
                       )}
-                      {block.external_link && (
-                        <div>
-                          <div className="flex mt-10">
-                            <ArrowTopRightOnSquareIcon
-                              className="h-8 w-8 mr-2"
-                              aria-hidden="true"
-                            />
-                            <h1 className="text-2xl font-semibold mb-5">外部リンク</h1>
-                          </div>
-                          <ExternalLinkItem url={block.external_link} />
-                        </div>
-                      )}
+                      {block.external_link && <ExternalLinkItem url={block.external_link} />}
                       {block.box_merit && (
                         <div className={`${styles.tab_merit_box} text-gray-700 flex items-center`}>
                           <HandThumbUpIcon
