4d607831d0d73ed4f4c9ced9572b837ade3ed42c
Arata <aratadayoon@yahoo.co.jp>
2025-02-15 23:28:45 +0900

    #234 refactor: Search

--- components/Sidebars/Elements/Search/index.tsx
+++ components/Sidebars/Elements/Search/index.tsx
@@ -1,19 +1,19 @@
 import { useTheme } from 'next-themes';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/solid';
 import SearchField from '../../../Elements/SearchField';
-import styles from './index.module.css';
 
 export default function Search() {
   const { theme } = useTheme();
+
   return (
-    <div className={`pt-8 px-4 border py-5 ${theme === 'dark' ? 'DarkTheme' : 'LightTheme'}`}>
-      <h1
-        className={`${styles.profile} text-2xl text-center font-semibold mb-5 flex justify-center`}
-      >
-        <MagnifyingGlassIcon className="h-8 w-8 mr-2" aria-hidden="true" />
-        キーワードで探す
-      </h1>
-      <SearchField />
-    </div>
+    <>
+      <div className={`pt-8 px-4 border py-5 ${theme === 'dark' ? 'DarkTheme' : 'LightTheme'}`}>
+        <div className={`text-2xl text-center font-semibold mb-5 flex justify-center`}>
+          <MagnifyingGlassIcon className="h-8 w-8 mr-2" />
+          キーワードで探す
+        </div>
+        <SearchField />
+      </div>
+    </>
   );
 }
