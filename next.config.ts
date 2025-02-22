370b8ab984c2e1a44eba899e1b1cd1762f517e19
Arata <aratadayoon@yahoo.co.jp>
2025-02-22 23:33:32 +0900

    fix: SSG対応

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import { withSentryConfig } from '@sentry/nextjs';
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'export',
   reactStrictMode: true,
   images: {
     remotePatterns: [
