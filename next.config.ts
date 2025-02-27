4d1fd36359af5a599cd10811a5f75bd26066c5c7
Arata <aratadayoon@yahoo.co.jp>
2025-02-27 17:03:15 +0900

    #234 refactor: rootFiles

--- next.config.ts
+++ next.config.ts
@@ -2,29 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.microcms-assets.io',
-        port: '',
-        pathname: '**',
-      },
-      {
-        protocol: 'http',
-        hostname: 'www.rentracks.jp',
-        port: '',
-        pathname: '/adx/**',
-      },
-      {
-        protocol: 'http',
-        hostname: 'www.image-rentracks.com',
-        port: '',
-        pathname: '/7876/**',
-      },
-    ],
-  },
 };
 
 export default nextConfig;
