5d5ee42947cde98b5400d44ec6077795a6b185d0
Arata <aratadayoon@yahoo.co.jp>
2025-02-27 13:48:18 +0900

    change: sitemap.xml

--- next-sitemap.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-
-module.exports = {
-  siteUrl: 'https://realunivlog.com/',
-  // generateRobotsTxt: true,
-  outDir: 'out',
-  sitemapSize: 7000,
-  exclude: ['/p/*', '/category/*/p/*', '/tag/*', '/archive/*', '/manifest.json'],
-};
---
+++ src/app/sitemap.ts
@@ -0,0 +1,85 @@
+import type { MetadataRoute } from 'next';
+import { getAllLists } from '@/libs/microcms';
+import { categories } from '@/section/category';
+
+export const dynamic = 'force-static';
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const articles = await getAllLists();
+
+  const url = process.env.NEXT_PUBLIC_BASE_URL;
+  const lastModified = new Date();
+  const changeFrequency = 'daily';
+  const priority = 0.7;
+
+  const appPages: MetadataRoute.Sitemap = [
+    {
+      url: `${url}`,
+      lastModified: lastModified,
+      changeFrequency: changeFrequency,
+      priority: priority,
+    },
+    {
+      url: `${url}/copyright`,
+      lastModified: lastModified,
+      changeFrequency: changeFrequency,
+      priority: priority,
+    },
+    {
+      url: `${url}/profile`,
+      lastModified: lastModified,
+      changeFrequency: changeFrequency,
+      priority: priority,
+    },
+    {
+      url: `${url}/privacy`,
+      lastModified: lastModified,
+      changeFrequency: changeFrequency,
+      priority: priority,
+    },
+    {
+      url: `${url}/contact`,
+      lastModified: lastModified,
+      changeFrequency: changeFrequency,
+      priority: priority,
+    },
+    {
+      url: `${url}/disclaimer`,
+      lastModified: lastModified,
+      changeFrequency: changeFrequency,
+      priority: priority,
+    },
+    {
+      url: `${url}/link`,
+      lastModified: lastModified,
+      changeFrequency: changeFrequency,
+      priority: priority,
+    },
+    {
+      url: `${url}/sitemap`,
+      lastModified: lastModified,
+      changeFrequency: changeFrequency,
+      priority: priority,
+    },
+  ];
+
+  const categoryPages: MetadataRoute.Sitemap = categories.map((category) => {
+    return {
+      url: `${url}/category/${category.id}`,
+      lastModified: lastModified,
+      changeFrequency: changeFrequency,
+      priority: priority,
+    };
+  });
+
+  const articlePages: MetadataRoute.Sitemap = articles.map((article) => {
+    return {
+      url: `${url}/articles/${article.id}`,
+      lastModified: article.updatedAt,
+      changeFrequency: changeFrequency,
+      priority: priority,
+    };
+  });
+
+  return [...appPages, ...categoryPages, ...articlePages];
+}
