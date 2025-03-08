5e35851fc1f71bef8c2390ff0fa36fcf06e1b682
Arata <aratadayoon@yahoo.co.jp>
2025-03-08 18:58:13 +0900

    refactor: *.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,20 +1,22 @@
-package api
+package main
 
 import (
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	"NextBlogApp/api"
 )
 
-func Main() {
+func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 
-	http.HandleFunc("/api/recaptcha", RecaptchaHandler)
-	http.HandleFunc("/api/sendemail", SendEmailHandler)
+	http.HandleFunc("/api/recaptcha", api.RecaptchaHandler)
+	http.HandleFunc("/api/sendemail", api.SendEmailHandler)
 
 	fmt.Printf("Server is running on port %s\n", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
--- cmd/server.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"NextBlogApp/api"
-)
-
-func main() {
-	api.Main()
-}
