aaeb5d91edeb4b4305b6fd7e611c00f4831c8b99
Arata <aratadayoon@yahoo.co.jp>
2025-04-07 18:41:25 +0900

    add: link RSS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,41 +50,30 @@ type Props = {
 };
 
 export default async function RootLayout({ children }: Props) {
+  const defaultUrl = process.env.NEXT_PUBLIC_BASE_URL;
+  const defaultTitle = process.env.NEXT_PUBLIC_BASE_TITLE;
+
+  const title = `${defaultTitle}`;
+  const url = `${defaultUrl}`;
+
   return (
     <html lang="ja" suppressHydrationWarning>
       <head>
         <meta name="format-detection" content="email=no,telephone=no,address=no" />
         <link rel="icon" href="/favicon.ico" />
-        <link
-          rel="icon"
-          href="https://realunivlog.com/images/head/16.png"
-          sizes="16x16"
-          type="image/png"
-        />
-        <link
-          rel="icon"
-          href="https://realunivlog.com/images/head/32.png"
-          sizes="32x32"
-          type="image/png"
-        />
-        <link
-          rel="icon"
-          href="https://realunivlog.com/images/head/48.png"
-          sizes="48x48"
-          type="image/png"
-        />
-        <link rel="apple-touch-icon" href="https://realunivlog.com/images/head/realstudent.png" />
-        <meta
-          name="msapplication-TileImage"
-          content="https://realunivlog.com/images/head/realstudent512.png"
-        />
+        <link rel="icon" href={`${url}/images/head/16.png`} sizes="16x16" type="image/png" />
+        <link rel="icon" href={`${url}/images/head/32.png`} sizes="32x32" type="image/png" />
+        <link rel="icon" href={`${url}/images/head/48.png`} sizes="48x48" type="image/png" />
+        <link rel="apple-touch-icon" href={`${url}/images/head/realstudent.png`} />
+        <meta name="msapplication-TileImage" content={`${url}/images/head/realstudent512.png`} />
         <meta name="msapplication-TileColor" content="#E0CBBA" />
         <meta property="og:type" content="blog" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:creator" content="@Aokumoblog" />
         <meta name="twitter:site" content="@Aokumoblog" />
-        <meta property="og:site_name" content="リアル大学生" />
+        <meta property="og:site_name" content={title} />
         <meta property="og:locale" content="ja_JP" />
+        <link rel="alternate" type="application/rss+xml" title={title} href={`${url}/rss.xml`} />
         <GoogleSearchConsole />
       </head>
       <body>
