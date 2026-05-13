f8f0ea89408a66a9d87cd28e73a1aa4edd2f0cae
Arata <aratadayoon@yahoo.co.jp>
2026-05-14 00:28:04 +0900

    add: category

--- src/components/Common/Layouts/Sidebar/Elements/Category/index.tsx
+++ src/components/Common/Layouts/Sidebar/Elements/Category/index.tsx
@@ -18,24 +18,8 @@ export default function Category() {
           <FolderIcon className="h-8 w-8 mr-2" />
           カテゴリー
         </div>
-        <div className="flex gap-4 mt-5 md:mt-5">
-          {CATEGORY_ARR.slice(0, 2).map((item) => (
-            <Link
-              key={item.name}
-              href={`/category/${item.id}`}
-              className={`${styles.CategoryList} text-start p-4 md:p-3 border shadow-lg hover:shadow-xl transition-shadow duration-200 transform hover:-translate-y-1 ${theme === 'dark' ? 'DarkTheme' : 'LightTheme'}`}
-            >
-              <div className="flex justify-center">
-                <item.icon className="w-12 h-12" />
-              </div>
-              <div className="flex justify-center mt-2">
-                <div>{item.name}</div>
-              </div>
-            </Link>
-          ))}
-        </div>
-        <div className="flex gap-4 mt-5 md:mt-5">
-          {CATEGORY_ARR.slice(2, 4).map((item) => (
+        <div className="grid grid-cols-2 gap-4 mt-5 md:mt-5">
+          {CATEGORY_ARR.map((item) => (
             <Link
               key={item.name}
               href={`/category/${item.id}`}
--- src/constants/category.ts
+++ src/constants/category.ts
@@ -1,9 +1,17 @@
 import { IoAirplaneOutline } from 'react-icons/io5';
-import { BookOpenIcon, CommandLineIcon, AcademicCapIcon } from '@heroicons/react/24/outline';
+import {
+  BookOpenIcon,
+  CommandLineIcon,
+  AcademicCapIcon,
+  SparklesIcon,
+  BriefcaseIcon,
+} from '@heroicons/react/24/outline';
 
 export const CATEGORY_ARR = [
-  { name: 'プログラミング', id: 'programming', icon: CommandLineIcon },
   { name: '大学生活', id: 'university', icon: AcademicCapIcon },
+  { name: '社会人生活', id: 'work', icon: BriefcaseIcon },
+  { name: 'レジャー', id: 'leisure', icon: SparklesIcon },
   { name: '旅行', id: 'travel', icon: IoAirplaneOutline },
+  { name: 'プログラミング', id: 'programming', icon: CommandLineIcon },
   { name: 'ブログ', id: 'blog', icon: BookOpenIcon },
 ];
